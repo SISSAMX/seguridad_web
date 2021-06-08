fe9e833049d4a31f3fc3719e125e76bcac79902a
Gabriela Razo Roldán <53321809+gabrielaRazo@users.noreply.github.com>
2021-06-08 15:54:01 -0500

    Categoría Featured Stories primera versión.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,23 +1,29 @@
 import React, {useContext} from 'react';
-import {Row, Col} from 'antd';
+import {Row, Col, Menu} from 'antd';
 import CarruselCategorias from '../common/components/Components/Dashboard/CarruselCategorias';
 import Tendencias from '../common/components/Components/Dashboard/Tendencias';
 import styles from '../styles/Home.module.css'
+import Categorías from '../common/components/Components/Dashboard/Categoria1';
 
 const Dashboard  = () =>(
     <>
     <div className={styles.espacioArribaDashboard}/>
-    <Col  span={17} offset={3}>
-        <Row>
-            <Col span={15}>
-                <CarruselCategorias/>
-            </Col>
-            <Col span={8} offset={1}>
-            <Tendencias/>
-            </Col>
-        </Row>
-    </Col>
-    <div className={styles.espacioAbajoDashboard}/>
+        <Col  span={17} offset={3}>
+            <Row>
+                <Col span={15}>
+                    <CarruselCategorias/>
+                </Col>
+                <Col span={8} offset={1}>
+                    <Tendencias/>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={15}>
+                    <Categorías/>
+                </Col>
+            </Row>
+        </Col>
+  {/*   <div className={styles.espacioAbajoDashboard}/> */}
     </>
 );
 
